3c4f1a988723617ee1b9e6a47da4f804e5677cff
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2019-02-24 16:51:10 -0500

    Set up file structure, start Rmd

---
+++ src/clean_data.R
@@ -0,0 +1,12 @@
+# Dependencies
+library(tidyverse)
+
+# Import data
+hate_2017 <- read_csv("../data/table-13_hate-crime-incidents_2017.csv")
+
+hate_2017
+
+
+# CLEAN -------------------------------------------------------------------
+
+# 
