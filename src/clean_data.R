34f9ddc371dc867ac69fad96d2050c6c99f1200f
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2019-02-24 18:57:39 -0500

    Loop to select incidents per state

--- src/clean_data.R
+++ src/clean_data.R
@@ -1,9 +1,10 @@
 # Dependencies
 library(tidyverse)
 
-# Import data
+# Find all files
 files <- list.files(path = "data/table-13_hate-crimes", pattern = "*.csv")
 
+# Import individual data frames
 for (i in seq_along(files)) {
   year <- str_extract(files[i], "([0-9]){4}")
   df <- read_csv(paste0("data/table-13_hate-crimes/", files[i]))
@@ -11,3 +12,20 @@ for (i in seq_along(files)) {
 }
 
 # CLEAN -------------------------------------------------------------------
+
+# List of all data frames
+df_list <- list(hate_2006, hate_2007, hate_2008, hate_2009, hate_2010, hate_2011,
+                hate_2012, hate_2013, hate_2014, hate_2015, hate_2016, hate_2017)
+
+# Remove missing values and useless column
+df_list2 <- lapply(df_list, function (df) {
+  df %>%
+    select(-place) %>%
+    filter(total == "Total")
+})
+
+# Extract data frames from list
+for (i in seq_along(df_list)) {
+  year <- str_extract(files[i], "([0-9]){4}")
+  assign(paste0("hate_", year), df_list2[[i]])
+}
