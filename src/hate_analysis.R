ffd80cffb7300ed341b3b35048fdf945b1c3160c
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2019-03-02 21:43:51 -0500

    Render animated maps, comparative choropleths

--- src/hate_analysis.R
+++ src/hate_analysis.R
@@ -3,10 +3,14 @@ library(cowplot)
 library(tidyverse)
 library(viridis)
 library(gganimate)
-library(ggrepel)
+library(gifski)
+library(fiftystater)
+library(sf)
+library(transformr)
 
 # Import data
 hate <- read_csv("data/hate_crimes.csv")
+couples <- read_csv("data/2010-census-same-sex-couples-state.csv")
 
 # Set defaults
 theme_set(theme_minimal())
@@ -28,7 +32,8 @@ so_total <- sum(so_total_year$total_so)
 # Total sexual orientation hate crimes over time
 so_crimes_time <- ggplot(so_total_year, aes(x = year, y = total_so)) +
   geom_line(color = "#440154FF", size = 1.25) +
-  scale_y_continuous(breaks = seq(1000, 1300, 50), limits = c(1000, 1300)) +
+  scale_y_continuous(breaks = seq(0, 1400, 200), limits = c(0, 1400),
+                     expand = c(0, 0), labels = scales::comma) +
   scale_x_continuous(breaks = seq(2006, 2017, 1), limits = c(2006, 2017),
                      minor_breaks = seq(1, 11, 1)) +
   labs(y = "Incidents",
@@ -40,6 +45,31 @@ so_crimes_time <- ggplot(so_total_year, aes(x = year, y = total_so)) +
 # Save plot
 ggsave("sexual-orientation-hate-crimes-time.png", plot = so_crimes_time, 
        path = "data/results/", width = 6.5, height = 5)
+
+# Animated line graph
+so_crimes_time_animate <- ggplot(so_total_year, aes(x = year, y = total_so)) +
+  geom_line(color = "#440154FF", size = 1.25) +
+  geom_point(size = 3) +
+  geom_point(size = 1.5, color = "white") +
+  scale_y_continuous(breaks = seq(0, 1400, 200), limits = c(0, 1400),
+                     expand = c(0, 0), labels = scales::comma) +
+  scale_x_continuous(breaks = seq(2006, 2017, 1), limits = c(2006, 2017),
+                     minor_breaks = seq(1, 11, 1)) +
+  labs(y = "Incidents",
+       x = "Year",
+       title = "Hate Crimes Based on Sexual Orientation",
+       subtitle = "United States, 2006—2017",
+       caption = "Source: FBI UCR Hate Crime Statistics") +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(size = 14),
+        plot.title = element_text(size = 20),
+        plot.subtitle = element_text(size = 16)) +
+  transition_reveal(year)
+
+# Save animation
+animate(so_crimes_time_animate, width = 700, height = 500)
+anim_save("sexual-orientation-hate-crimes-time.gif",
+          animation = last_animation(), path = "data/results/")
 
 # PROPORTION OF HATE CRIMES -----------------------------------------------
 
@@ -70,7 +100,7 @@ lgbt_total <- hate %>%
   pull()
 
 # LGB vs Straight Individuals
-(lgb_v_straight <- data.frame(sexuality = c("Lesbian, Gay, Bisexual", "Straight"),
+lgb_v_straight <- data.frame(sexuality = c("Lesbian, Gay, Bisexual", "Straight"),
                              percent = c(0.035, 0.965)) %>%
   ggplot(aes(x = "", y = percent, fill = sexuality)) +
   geom_col() +
@@ -93,10 +123,9 @@ lgbt_total <- hate %>%
         axis.ticks.y = element_blank(),
         panel.grid = element_blank(),
         legend.position = "none")
-)
 
 # LGB vs Other Incidents
-(lgb_v_other <- data.frame(type = c("Lesbian, Gay, Bisexual", "Other"),
+lgb_v_other <- data.frame(type = c("Lesbian, Gay, Bisexual", "Other"),
                           n = c(so_total, all_total)) %>%
   ggplot(aes(x = "", y = n, fill = type)) +
   geom_col() +
@@ -119,7 +148,6 @@ lgbt_total <- hate %>%
         axis.ticks.y = element_blank(),
         panel.grid = element_blank(),
         legend.position = "none")
-)
 
 # Combine proportional plots
 lgb_pop_hate <- plot_grid(lgb_v_straight, lgb_v_other)
@@ -128,7 +156,151 @@ lgb_pop_hate <- plot_grid(lgb_v_straight, lgb_v_other)
 ggsave("lgb-population-vs-hate-crimes.png", plot = lgb_pop_hate,
        path = "data/results/", width = 10, height = 5)
 
-#
-all_total_year %>%
+# OTHER VS. SO BY YEAR ----------------------------------------------------
+
+# Other hate crimes by year
+other_total_year <- hate %>%
+  filter(bias != "sexual_orientation") %>%
+  group_by(bias, year) %>%
+  summarize(total = sum(incidents)) %>%
+  group_by(year) %>%
+  summarize(total_incidents = sum(total))
+
+# Total vs. sexual orientation by year
+hate_other_v_so <- other_total_year %>%
   left_join(so_total_year, by = "year") %>%
-  mutate(prop_so = total_so / total_incidents)
+  gather(key = totals, value = n, -year) %>%
+  mutate(
+    year = as.integer(year),
+    totals = fct_recode(
+      factor(totals),
+      "Other" = "total_incidents",
+      "Sexual Orientation" = "total_so"
+  )) %>%
+  ggplot(aes(x = totals, y = n, fill = totals)) +
+  geom_col() +
+  scale_y_continuous(breaks = seq(0, 7000, 1000),
+                     labels = scales::comma) +
+  scale_fill_manual(values = c("#440154FF", "#26828EFF")) +
+  labs(title = "Hate Crimes by Year: {frame_time}",
+       fill = "",
+       x = "",
+       y = "Incidents",
+       caption = "Source: FBI UCR Hate Crime Statistics") +
+  theme(legend.position = "none",
+        axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18),
+        plot.title = element_text(size = 20)) +
+  transition_time(year)
+
+# Save animation
+animate(hate_other_v_so, width = 700, height = 500)
+anim_save("hate-crime-incidents-other-v-sexual-orientation.gif",
+          animation = last_animation(), path = "data/results/")
+
+# CREATE BASIC SF VECTOR MAP ----------------------------------------------
+
+# Convert point-based data frame into simple features
+sf_fifty <- st_as_sf(fifty_states, coords = c("long", "lat")) %>%
+  # Group simple features and transform into polygons
+  group_by(id, piece) %>%
+  summarize(do_union = FALSE) %>%
+  st_cast("POLYGON") %>%
+  # Group polygons into multipolygons
+  group_by(id) %>%
+  summarize()
+
+# Set projection method
+st_crs(sf_fifty) <- 4326
+
+# Render vector map
+ggplot(sf_fifty) +
+  geom_sf() +
+  coord_sf(crs = "+proj=aea +lat_1=25 +lat_2=50 +lon_0=-100")
+
+# MAPS OVER TIME ----------------------------------------------------------
+
+# Prepare key column
+sf_fifty$id <- str_to_title(sf_fifty$id)
+couples$state <- str_to_title(couples$state)
+
+# Merge data frames
+sf_hate <- left_join(sf_fifty, hate, by = c("id" = "state"))
+sf_couples <- left_join(sf_fifty, couples, by = c("id" = "state"))
+
+# Render choropleth for population
+same_sex_couples_state_2010 <- ggplot(sf_couples) +
+  geom_sf(aes(fill = n_couples), color = "white") +
+  coord_sf(crs = "+proj=aea +lat_1=25 +lat_2=50 +lon_0=-100", ndiscr = 0) +
+  scale_fill_viridis_c(option = "viridis", labels = scales::comma) +
+  labs(title = "Number of Same-Sex Couples by State, 2010",
+       caption = "Source: 2010 U.S. Census",
+       fill = "") +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position = "top",
+        legend.key.width = unit(3, "cm"),
+        legend.key.height = unit(0.2, "cm"),
+        plot.title = element_text(hjust = 0.5))
+
+# Render choropleth for hate crimes in 2010
+hate_across_states_2010 <- sf_hate %>%
+  filter(bias == "sexual_orientation", year == 2010) %>%
+  ggplot() +
+  geom_sf(aes(fill = incidents), color = "white") +
+  coord_sf(crs = "+proj=aea +lat_1=25 +lat_2=50 +lon_0=-100", ndiscr = 0) +
+  scale_fill_viridis_c(option = "viridis", labels = scales::comma) +
+  labs(title = "Number of Hate Crimes Against LGB Individuals by State, 2010",
+       caption = "Source: FBI UCR Hate Crime Statistics",
+       fill = "") +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position = "top",
+        legend.key.width = unit(3, "cm"),
+        legend.key.height = unit(0.2, "cm"),
+        plot.title = element_text(hjust = 0.5))
+
+# Combine choropleths
+two_maps <- plot_grid(same_sex_couples_state_2010, hate_across_states_2010, ncol = 1, align = "v")
+
+# Save combined choropleths
+ggsave("lgb-population-vs-hate-crimes-across-states.png", plot = two_maps,
+       path = "data/results/", width = 7, height = 10)
+
+# Render animated choropleth
+hate_across_states <- sf_hate %>%
+  filter(bias == "sexual_orientation") %>%
+  mutate(year = as.integer(year)) %>%
+  ggplot() +
+  geom_sf(aes(fill = incidents), color = "white") +
+  coord_sf(crs = "+proj=aea +lat_1=25 +lat_2=50 +lon_0=-100", ndiscr = 0) +
+  scale_fill_viridis_c(option = "viridis", labels = scales::comma) +
+  labs(title = "Hate Crimes Against LGB Individuals Across States",
+       subtitle = "Year: {frame_time}",
+       caption = "Source: FBI UCR Hate Crime Statistics",
+       fill = "") +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position = "top",
+        legend.key.width = unit(3, "cm"),
+        legend.key.height = unit(0.2, "cm"),
+        plot.title = element_text(hjust = 0.5, size = 18),
+        plot.subtitle = element_text(hjust = 0.5, size = 18)) +
+  transition_time(year)
+
+# Save animation
+animate(hate_across_states, width = 700, height = 500)
+anim_save("hate-crimes-against-lgb-across-states.gif",
+          animation = last_animation(), path = "data/results/")
